6abb4c3126bd7b76af852583c2370a15b866fa0b
Kevin Jose Gomez Sanchez <kevingomezsh25@gmail.com>
2025-12-05 23:01:49 -0400

    FIX: Corregida configuracion de vite.config.js para resolver error de entry module

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// El nombre del repositorio es 'Proyecto-de-programacion---grupo-5-'
+const repoName = '/Proyecto-de-programacion---grupo-5-/' 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  // ¡AÑADE ESTA LÍNEA O CAMBIA SU VALOR ACTUAL!
-  base: '/Proyecto-de-programacion---grupo-5-/', 
+  // BASE: Corrige la ruta base para GitHub Pages
+  base: repoName, 
+  
   plugins: [react()],
+  
+  // BUILD: Asegura que el index.html se encuentre y se use
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html', // Indica explícitamente el punto de entrada
+      },
+    },
+  },
 })
