fc70d315e0a3d8e2da11e111cc805524ed6a4943
Kevin Jose Gomez Sanchez <kevingomezsh25@gmail.com>
2025-12-05 22:21:34 -0400

    Compilacion final exitosa y archivos de produccion (dist) subidos

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // ¡AÑADE ESTA LÍNEA O CAMBIA SU VALOR ACTUAL!
+  base: '/Proyecto-de-programacion---grupo-5-/', 
   plugins: [react()],
 })
